717cd42bb45e68388512f59eb8cd915ee9396c1a
Дария Долгая <nettyshki@gmail.com>
2018-03-22 21:51:11 +0200

    l2

---
+++ l2_vars_and_types/examples.py
@@ -0,0 +1,17 @@
+
+name = input ("Enter your name ")
+phone_number = input ('Enter your phone please ')
+group = 4
+print (f'{name}:{phone_number} {group}')
+
+#test file for lesson 2
+my_list1 = []
+temp_list = [5678,67.8,[23,46,'try','jump'],('you','me',45),'you are the best', {'John': 'student','Jane':'teacher'}]
+for item in temp_list:
+    my_list1.append(item)
+for item in my_list1:
+    print (type(item))
+"""
+this is my cool comment
+yeah it rocks
+"""
